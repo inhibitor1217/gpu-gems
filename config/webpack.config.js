f2f555a3b8d679004347b6342c8fdbc4ba9df000
dugi <dugi@channel.io>
2021-07-11 18:05:17 +0900

    configure webpack and webpack-dev-server

---
+++ config/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: path.join(process.cwd(), 'src', 'index.ts'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(process.cwd(), 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+    modules: [
+      path.resolve(process.cwd(), 'src'),
+      path.resolve(process.cwd(), 'node_modules'),
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(process.cwd(), 'public'),
+    compress: true,
+    port: 3000,
+  }
+}
