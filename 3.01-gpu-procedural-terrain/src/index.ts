b8ff97849229e00c2d7bafd578088c9d8565eb0d
dugi <dugi@channel.io>
2021-07-15 02:31:36 +0900

    setup webgpu environment

--- 3.01-gpu-procedural-terrain/src/index.ts
+++ 3.01-gpu-procedural-terrain/src/index.ts
@@ -1,3 +1,22 @@
-console.log('Hello, world!')
+async function main() {
+  if (!navigator.gpu) {
+    console.warn('WebGPU is not supported or enabled in this browser.')
+    return
+  }
+
+  const adapter = await navigator.gpu.requestAdapter()
+  const device = await adapter?.requestDevice()
+
+  const canvas = document.getElementById('renderCanvas')
+
+  if (!canvas || !(canvas instanceof HTMLCanvasElement)) {
+    console.warn('HTMLCanvasElement with id #renderCanvas not found.')
+    return
+  }
+
+  const context = canvas.getContext('webgpu')
+}
+
+main()
 
 export default {}
