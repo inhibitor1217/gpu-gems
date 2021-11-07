6e7fe9b19ef83b88d26a2649c9a00948c0c430e8
dugi <dugi@channel.io>
2021-11-07 14:01:08 +0900

    setup types directory

--- app/webpack/base.config.js
+++ app/webpack/base.config.js
@@ -26,6 +26,7 @@ module.exports = {
       'Common': path.resolve(__dirname, '..', 'src', 'common'),
       'Routes': path.resolve(__dirname, '..', 'src', 'routes'),
       'Styles': path.resolve(__dirname, '..', 'src', 'styles'),
+      'Types': path.resolve(__dirname, '..', 'src', 'types'),
       'Util': path.resolve(__dirname, '..', 'src', 'util'),
     },
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
