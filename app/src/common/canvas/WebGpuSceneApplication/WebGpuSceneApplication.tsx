8eba6f03bbff2cced355f44052bd75da7ed212af
dugi <dugi@channel.io>
2021-11-07 23:23:09 +0900

    revive runApplication effect

--- app/src/common/canvas/WebGpuSceneApplication/WebGpuSceneApplication.tsx
+++ app/src/common/canvas/WebGpuSceneApplication/WebGpuSceneApplication.tsx
@@ -1,11 +1,29 @@
-import { useContext } from 'react'
+import {
+  useContext,
+  useEffect,
+} from 'react'
 
 import EngineContext from 'Contexts/EngineContext'
+import { O } from 'Util/Fx'
 import type SceneApplication from 'Util/SceneApplication'
 
 const WebGpuSceneApplication = (app: SceneApplication.SceneApplication) =>
   function Application() {
-    const engine = useContext(EngineContext)
+    const engineT = useContext(EngineContext)
+
+    useEffect(function runApplication() {
+      if (!engineT.fulfilled()) { return }
+      if (O.isNone(engineT.data)) { return }
+
+      const engine = engineT.data
+      const sceneP = app.createScene(engine)
+
+      sceneP.then(scene => engine.runRenderLoop(() => scene.render()))
+
+      return function cleanup() {
+        sceneP.then(scene => scene.dispose())
+      }
+    }, [engineT])
 
     return null
   }
