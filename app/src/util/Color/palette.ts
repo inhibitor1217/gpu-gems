e8999591178135d969c8ddf364aad0d6c978feaa
dugi <dugi@channel.io>
2021-11-29 00:21:33 +0900

    feat(util/color): add palette to uint8 and float32 parsers

---
+++ app/src/util/Color/palette.ts
@@ -0,0 +1,5 @@
+import { LightFoundation, SemanticNames } from '@channel.io/bezier-react'
+
+import type { PaletteColor } from './type'
+
+export const palette = LightFoundation.theme as Record<SemanticNames, PaletteColor>
---
+++ app/src/util/Color/parse.ts
@@ -0,0 +1,16 @@
+import { A, F } from 'Util/Fx'
+
+import type { PaletteColor } from './type'
+
+const BYTE_MAX = 255
+
+const rgb = (color: PaletteColor): number[] =>
+  [[0, 2], [2, 4], [4, 6]]
+    .map(([s, e]) => color.substring(1).substring(s, e))
+    .map(c => parseInt(c, 16))
+
+const rgba = F.pipe(rgb, c => [...c, BYTE_MAX])
+
+export const parseToRGBAInteger = rgba
+
+export const parseToRGBA = F.pipe(rgba, colors => colors.map(c => c / BYTE_MAX))
