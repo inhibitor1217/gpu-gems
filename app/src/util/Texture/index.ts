bbae1f2d7d3ba0e8f330aff9748c0df04813f7b1
dugi <dugi@channel.io>
2021-11-29 00:21:53 +0900

    feat(util/texture): add monochromeMosaic texture

---
+++ app/src/util/Texture/index.ts
@@ -0,0 +1,1 @@
+export { default as monochromeMosaic } from './monochromeMosaic'
---
+++ app/src/util/Texture/monochromeMosaic.ts
@@ -0,0 +1,29 @@
+import {
+  RawTexture,
+  Scene,
+  Texture,
+} from '@babylonjs/core'
+
+import * as Color from 'Util/Color'
+
+const RESOLUTION = 2
+const MONOCHROME_MOSAIC_BUFFER = () => new Uint8Array([
+    ...Color.parseToRGBAInteger(Color.palette['bg-grey-darkest']),
+    ...Color.parseToRGBAInteger(Color.palette['bg-grey-lightest']),
+    ...Color.parseToRGBAInteger(Color.palette['bg-grey-lightest']),
+    ...Color.parseToRGBAInteger(Color.palette['bg-grey-darkest']),
+  ])
+
+const monochromeMosaic = (scene: Scene): Texture =>
+  RawTexture.CreateRGBATexture(
+    MONOCHROME_MOSAIC_BUFFER(),
+    RESOLUTION,
+    RESOLUTION,
+    scene,
+    false,
+    undefined,
+    Texture.NEAREST_NEAREST,
+    undefined,
+  )
+
+export default monochromeMosaic
