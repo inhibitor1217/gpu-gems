e3a17d083a638d5a0ad9c3075dd6d2cd75540657
dugi <dugi@channel.io>
2021-11-30 23:23:53 +0900

    chore(eslint): adjust eslint rules

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -10,6 +10,14 @@ module.exports = _.merge(
     ],
     parserOptions: {
       project: './tsconfig.json',
-    }
+    },
+    rules: {
+      '@typescript-eslint/semi': ['error', 'never'],
+      'array-callback-return': 'off',
+      'implicit-arrow-linebreak': 'off',
+      'react/function-component-definition': 'off',
+      "react/jsx-uses-react": 'off',
+      "react/react-in-jsx-scope": 'off',
+    },
   }
 )
