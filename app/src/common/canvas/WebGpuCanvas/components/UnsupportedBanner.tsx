5c943c82887d5cb17a82d956d8c81b7a2d39d1ae
dugi <dugi@channel.io>
2022-01-23 13:47:18 +0900

    chore(deps): update @channel.io/bezier-react

--- app/src/common/canvas/WebGpuCanvas/components/UnsupportedBanner.tsx
+++ app/src/common/canvas/WebGpuCanvas/components/UnsupportedBanner.tsx
@@ -1,15 +1,15 @@
 import { memo } from 'react'
 import {
   Banner,
-  BannerColorVariant,
+  BannerVariant,
 } from '@channel.io/bezier-react'
 
 function UnsupportedBanner() {
   return (
     <Banner
       icon="block"
-      colorVariant={BannerColorVariant.Red}
-      text={`WebGPU is not supported in this browser.
+      variant={BannerVariant.Red}
+      content={`WebGPU is not supported in this browser.
 Refer to Chrome Platform Status to check current status of WebGPU support.`}
       hasLink
       linkText="See More"
