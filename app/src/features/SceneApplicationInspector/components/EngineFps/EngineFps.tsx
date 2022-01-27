9b5841c0e1d3077571f8b3fb94ebee0638772062
dugi <dugi@channel.io>
2022-01-27 18:41:59 +0900

    feat(enginefps): round up FPS to 3 digits

--- app/src/features/SceneApplicationInspector/components/EngineFps/EngineFps.tsx
+++ app/src/features/SceneApplicationInspector/components/EngineFps/EngineFps.tsx
@@ -2,7 +2,7 @@ import useEngineFps from '../../hooks/useEngineFps'
 import { FPS } from '../../types/fps'
 import { InspectorField } from '../InspectorField'
 
-const mapFps = (value: FPS) => value.toString()
+const mapFps = (value: FPS) => value.toFixed(3).toString()
 
 function EngineFps() {
   const fps = useEngineFps()
