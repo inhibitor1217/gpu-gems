ea30e303ae9ff2acf520e4e23f8b248a80c22564
dugi <dugi@channel.io>
2022-02-13 11:24:21 +0900

    feat: setup redux and react-redux

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
+import { Provider as ReduxStoreProvider } from 'react-redux'
 
 import GlobalStyles from 'Styles/global'
 
 import App from './App'
+import { store } from './store'
 
 ReactDOM.render(
   <StrictMode>
     <GlobalStyles />
-    <App />
+    <ReduxStoreProvider store={store}>
+      <App />
+    </ReduxStoreProvider>
   </StrictMode>,
   document.getElementById('root'),
 )
---
+++ app/src/store/index.ts
@@ -0,0 +1,8 @@
+import {
+  combineReducers,
+  configureStore,
+} from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  reducer: combineReducers({}),
+})
