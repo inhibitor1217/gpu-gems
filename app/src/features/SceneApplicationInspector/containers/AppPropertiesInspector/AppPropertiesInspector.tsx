6725e31a3f05d01f6db76a21c59e6309dd3b32e4
dugi <dugi@channel.io>
2022-02-14 00:16:10 +0900

    feat(apppropertiesinspector): add property fields

--- app/src/features/SceneApplicationInspector/containers/AppPropertiesInspector/AppPropertiesInspector.tsx
+++ app/src/features/SceneApplicationInspector/containers/AppPropertiesInspector/AppPropertiesInspector.tsx
@@ -1,20 +1,48 @@
 import { SectionLabel } from '@channel.io/bezier-react'
+import _ from 'lodash'
 
 import { Card } from 'Common/base/Card'
+import { InspectorField } from 'Features/SceneApplicationInspector/components/InspectorField'
+import { useSelector } from 'Redux/store'
 
 function Title() {
   return (
     <SectionLabel
-      leftContent={{ icon: 'number' }}
+      leftContent={{ icon: 'info-filled' }}
       content="Properties Inspector"
     />
   )
 }
 
+interface PropertyProps {
+  name: string
+}
+
+const mapProperty = (value: any) => value.toString()
+
+function Property({ name }: PropertyProps) {
+  const current = useSelector((state) => _.get(state.applicationProperty, name))
+
+  return (
+    <InspectorField
+      keyIcon="number"
+      keyName={name}
+      value={current}
+      mapValue={mapProperty}
+    />
+  )
+}
+
 function AppPropertiesInspector() {
+  const names = useSelector(
+    (state) => _.keys(state.applicationProperty),
+    _.isEqual,
+  )
+
   return (
     <Card>
       <Title />
+      { names.map((name) => <Property key={name} name={name} />) }
     </Card>
   )
 }
--- app/src/redux/store.ts
+++ app/src/redux/store.ts
@@ -3,13 +3,23 @@ import {
   configureStore,
 } from '@reduxjs/toolkit'
 import logger from 'redux-logger'
+import {
+  TypedUseSelectorHook,
+  useSelector as useSelectorBase,
+} from 'react-redux'
 
 import { reducer as applicationProperty } from './modules/applicationProperty'
 
+const reducer = combineReducers({
+  applicationProperty,
+})
+
+type RootState = ReturnType<typeof reducer>
+
+export const useSelector: TypedUseSelectorHook<RootState> = useSelectorBase
+
 const store = configureStore({
-  reducer: combineReducers({
-    applicationProperty,
-  }),
+  reducer,
   middleware: (defaultMiddleware) => defaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV !== 'production',
 })
